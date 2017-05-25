c28af210704dce2df92e451de17f5ab13b55101d
wallnerdavid <wallner.david97@gmail.com>
2017-05-25 13:57:40 +0200

    index, login, main

--- app.py
+++ app.py
@@ -5,10 +5,15 @@ app.debug = True
 
 
 # http://flask.pocoo.org
+# az index fuggveny rendeleli le az index.html file-t
 
 @app.route("/")
 def hello():
     return render_template('index.html')
+
+@app.route("/login")
+def loginPage():
+    return render_template('login.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
