1a45996e6a92e19961730e0acfe528ad4a6217b5
wallnerdavid <wallner.david97@gmail.com>
2017-06-08 08:51:00 +0200

    ez már a javított de még lehet kell javítani

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,9 @@
+/*jshint esnext: true */
+/*jshint devel: true */
+/*jslint node: true */
+/*jslint browser: true */
+/*jslint jquery: true */
+
 
   function switchMenu(clickedId) {
      console.log(clickedId+" megnyomva....");
@@ -14,9 +20,20 @@
 
 function addMunkanap() {
 	$('#munkanapItems').append(`
-	<div>HELLO</div>
+	    <div class="new_munkanap row">
+        <button class="remove btn btn-danger" onclick="removeMunkanap(this.id)">-</button>
+        <div class="col-xs-6 col-sm-2">
+        <input type="text" placeholder="datum" class="datum_mezo form-control" >
+      </div>
+        <div class="col-xs-6 col-sm-2">
+        <input type="number" min="0.5" max="12" step="0.5" class="munkaora form-control" placeholder="munkaóra">
+      </div>
+        <div class="col-xs-12 col-sm-6">
+        <textarea rows="3" class="form-control" placeholder="megjegyzés"></textarea>
+      </div>
+    </div>
 
-		`);
+	`);
 
 }
 
